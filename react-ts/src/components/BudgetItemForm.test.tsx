e19d590e5aba61a269176fad5bf7607e91646c3b
Robert Carrier <robmcarrier@gmail.com>
2023-01-16 17:44:03 -0600

    Fix js skip yml and update verbiage on test

--- react-ts/src/components/BudgetItemForm.test.tsx
+++ react-ts/src/components/BudgetItemForm.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import BudgetItemForm from './BudgetItemForm';
 describe('BudgetItemForm component', () => {
-  it('should render BudgetItemForm component correctly', () => {
+  it('should render the Button component correctly', () => {
     render(<BudgetItemForm />);
     const element = screen.getByRole('button');
     expect(element).toBeInTheDocument();
