769ddc63d7eb5388e919c723e85563b52719c150
Robert Carrier <robmcarrier@gmail.com>
2023-01-16 14:25:54 -0600

    Add tests

---
+++ src/test/java/com/robmcarrier/budgetapp/services/BudgetItemServiceTest.java
@@ -0,0 +1,20 @@
+package com.robmcarrier.budgetapp.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BudgetItemServiceTest {
+
+    @Test
+    void getBudgetItems() {
+    }
+
+    @Test
+    void createBudgetItem() {
+    }
+
+    @Test
+    void deleteBudgetItem() {
+    }
+}
