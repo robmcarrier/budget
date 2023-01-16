8493591f608476e7487f5a959656b2cda456f251
Robert Carrier <robmcarrier@gmail.com>
2023-01-16 10:03:06 -0600

    Update getBudgetItems and add createBudgetItem tests

--- src/test/java/com/robmcarrier/budgetapp/controllers/MainControllerTest.java
+++ src/test/java/com/robmcarrier/budgetapp/controllers/MainControllerTest.java
@@ -2,16 +2,20 @@ package com.robmcarrier.budgetapp.controllers;
 
 import com.robmcarrier.budgetapp.models.BudgetItem;
 import com.robmcarrier.budgetapp.services.BudgetItemService;
+import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @GraphQlTest(MainController.class)
@@ -26,35 +30,93 @@ class MainControllerTest {
     @Test
     void budgetItems() {
         List<BudgetItem> budgetItems = new ArrayList<>();
-        BudgetItem budgetItem1 = new BudgetItem();
-        budgetItem1.setId("12345");
-        budgetItem1.setName("test1");
-        budgetItem1.setAmount(100);
-        budgetItem1.setDayOfMonth(11);
+
+        BudgetItem budgetItem1 = createTestBudgetItem();
+        BudgetItem budgetItem2 = createTestBudgetItem();
+        BudgetItem budgetItem3 = createTestBudgetItem();
+        BudgetItem budgetItem4 = createTestBudgetItem();
+
         budgetItems.add(budgetItem1);
+        budgetItems.add(budgetItem2);
+        budgetItems.add(budgetItem3);
+        budgetItems.add(budgetItem4);
 
         Flux<BudgetItem> budgetItemFlux = Flux.fromIterable(budgetItems);
         String document = "query {" +
                 "  budgetItems {" +
                 "   id" +
                 "   name" +
+                "   amount" +
+                "   dayOfMonth" +
                 "  }" +
                 "}";
 
         given(budgetItemService.getBudgetItems()).willReturn(budgetItemFlux);
 
-        graphQlTester.document(document)
+        List<BudgetItem> responseItems = graphQlTester.document(document)
                 .execute()
-                .path("budgetItems[0]")
-                .entity(BudgetItem.class)
-                .equals(budgetItem1);
+                .path("budgetItems")
+                .entityList(BudgetItem.class)
+                .get();
+
+        assertThat(responseItems.size()).isEqualTo(4);
+
+        assertThat(budgetItem1.equals(responseItems.get(0))).isTrue();
+        assertThat(budgetItem2.equals(responseItems.get(1))).isTrue();
+        assertThat(budgetItem3.equals(responseItems.get(2))).isTrue();
+        assertThat(budgetItem4.equals(responseItems.get(3))).isTrue();
     }
 
     @Test
     void createBudgetItem() {
+        BudgetItem budgetItem = createTestBudgetItem();
+        BudgetItem request = new BudgetItem();
+        request.setName(budgetItem.getName());
+        request.setDayOfMonth(budgetItem.getDayOfMonth());
+        request.setSplit(budgetItem.isSplit());
+        request.setAmount(budgetItem.getAmount());
+
+        given(budgetItemService.createBudgetItem(request)).willReturn(Mono.just(budgetItem));
+
+        String document = "mutation($name: String! $amount: Int $dayOfMonth: Int $split: Boolean) {" +
+                "  createBudgetItem(budgetItem: {" +
+                "   name: $name," +
+                "   amount: $amount," +
+                "   dayOfMonth: $dayOfMonth," +
+                "   split: $split" +
+                "   }) {" +
+                "   id" +
+                "   name" +
+                "  }" +
+                "}";
+
+        BudgetItem responseItem = graphQlTester.document(document)
+                .variable("name", budgetItem.getName())
+                .variable("amount", budgetItem.getAmount())
+                .variable("dayOfMonth", budgetItem.getDayOfMonth())
+                .variable("split", budgetItem.isSplit())
+                .execute()
+                .path("createBudgetItem")
+                .entity(BudgetItem.class)
+                .get();
+
+        assertThat(responseItem.getName()).isEqualTo(budgetItem.getName());
+        assertThat(responseItem.getId()).isEqualTo(budgetItem.getId());
+        assertThat(responseItem.getAmount()).isEqualTo(0);
     }
 
     @Test
     void deleteBudgetItem() {
+    }
+
+    private BudgetItem createTestBudgetItem() {
+        Faker faker = new Faker();
+        BudgetItem budgetItem = new BudgetItem();
+        budgetItem.setId(faker.internet().uuid());
+        budgetItem.setName(faker.appliance().brand());
+        budgetItem.setAmount(new Random().nextInt(1000 - 10 + 1) + 10);
+        budgetItem.setDayOfMonth(new Random().nextInt(31 - 1 + 1) + 1);
+        budgetItem.setSplit(false);
+        return budgetItem;
     }
 }
