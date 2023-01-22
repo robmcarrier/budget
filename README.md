377d881bbbe1d67580bf35044bf64161b465d23e
Robert Carrier <robmcarrier@gmail.com>
2023-01-22 14:18:27 -0600

    Add react coverage and action

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 JAVA: <img src="https://github.com/robmcarrier/budget/blob/main/.github/badges/jacoco.svg" alt="Java code coverage"/>
+React: ![Lines](https://img.shields.io/badge/coverage-60.6%25-red.svg?style=flat)
--- react-ts/vitest.config.ts
+++ react-ts/vitest.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
+// @ts-ignore
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './setupTests.ts',
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'json', 'html', 'json-summary'],
+    },
   },
 });
